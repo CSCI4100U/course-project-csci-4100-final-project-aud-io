29791f5663e7c78e2282ccf7da7f235a05b40d45
rajiv-williams <rajiv.williams@ontariotechu.net>
2022-10-25 10:52:00 -0400

    Added basic UI for main page

--- group_project/lib/main.dart
+++ group_project/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'nav.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.purple,
       ),
-      home: const MyHomePage(title: 'Aud.io Home Page'),
+      home: const MyHomePage(title: 'Aud.io'),
     );
   }
 }
@@ -30,14 +31,28 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
+
+
   @override
   Widget build(BuildContext context) {
 
+    //Replacement can be MenuObjects class of name, route,
+    List options = ["Login/Logout","Profile","Friends","Playlists"];
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
+        actions: [
+          IconButton(
+            onPressed: (){},
+            icon: Icon(Icons.home),
+          ),
+          IconButton(
+            onPressed: (){},
+            icon: Icon(Icons.settings_outlined),
+          ),
+        ],
       ),
       body: Center(
         child: Column(
@@ -48,6 +63,39 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
+      drawer: Drawer(
+        child: Container(
+          padding: const EdgeInsets.all(50),
+          child: ListView.separated(
+              itemBuilder: (context,index){
+                return Container(
+                  height: 50,
+                  child: TextButton(
+                    child: Text("${options[index]}",
+                      style: const TextStyle(
+                          color: Colors.black
+                      ),
+                    ),
+                    onPressed: (){
+                      print("You pressed button ${options[index]}!");
+                    },
+                  ),
+                );
+              },
+              separatorBuilder: (context,index){
+                return Divider();
+              },
+              itemCount: options.length
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        child: const Icon(Icons.add),
+
+      )
+    ,
     );
+
   }
 }
