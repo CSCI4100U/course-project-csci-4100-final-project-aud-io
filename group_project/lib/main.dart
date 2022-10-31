73f23f8d42f328427e83ea8df144a1bbbec40499
rajiv-williams <rajiv.williams@ontariotechu.net>
2022-10-30 22:31:40 -0400

    Delete Dialog for Friends added.

--- group_project/lib/main.dart
+++ group_project/lib/main.dart
@@ -85,7 +85,7 @@ class _MyHomePageState extends State<MyHomePage> {
     ];
     return Scaffold(
       appBar: AppBar(
-        toolbarHeight: 100,
+        toolbarHeight: 120,
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: widget.title,
--- group_project/lib/user_classes/friends.dart
+++ group_project/lib/user_classes/friends.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'profile.dart';
 
 class FriendList extends StatefulWidget {
   const FriendList({Key? key,this.title}) : super(key: key);
@@ -10,6 +11,25 @@ class FriendList extends StatefulWidget {
 }
 
 class _FriendListState extends State<FriendList> {
+  int selectedIndex = 0;
+  List<Profile> allFriends = [
+    Profile(userName: "rajiv45",
+        phoneNum: "555-1234",
+        country: "Bahamas",
+        city: "Nassau"
+    ),
+    Profile(userName: "fab",
+        phoneNum: "555-1235",
+        country: "Canada",
+        city: "Ottawa"
+    ),
+    Profile(userName: "john_doe",
+        phoneNum: "555-1237",
+        country: "England",
+        city: "Liverpool"
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,25 +38,40 @@ class _FriendListState extends State<FriendList> {
         actions: [
           IconButton(
               onPressed: (){
-                //call _deleteFriend()
+                //Call async function that goes to route "/home"
+                Navigator.pushNamed(context, '/home');
               },
-              tooltip: 'Delete Friend',
-              icon: const Icon(Icons.delete)
+              tooltip: 'Home',
+              icon: const Icon(Icons.home)
           ),
         ],
       ),
-      body: ListView(
-        children: [
-          Container(
-            child: Text("Friend 1"),
-          ),
-          Container(
-            child: Text("Friend 2"),
-          ),
-          Container(
-            child: Text("Friend 3"),
-          ),
-        ],
+      body: ListView.builder(
+          padding: const EdgeInsets.all(8.0),
+          itemCount: allFriends.length,
+          itemBuilder: (context,index){
+            return Container(
+                  // decoration: BoxDecoration(color: gradeColors[index]),
+                  padding: const EdgeInsets.all(10.0),
+
+                  child: ListTile(
+                      title: Text("${allFriends[index].userName}",
+                        style: TextStyle(fontSize: 30),),
+                      trailing: GestureDetector(
+                          child: Icon(Icons.delete),
+                          onTap: (){
+                            Profile user = allFriends[index];
+                            //open dialog
+                            // "Are you sure you want to delete {user}"
+                              _showDeleteFriendAlert(context,user);
+
+                            },
+                      ),
+
+                  ),
+
+                );
+          }
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: (){
@@ -45,6 +80,40 @@ class _FriendListState extends State<FriendList> {
         tooltip: 'Add Friend',
         child: const Icon(Icons.add),
       ),
+    );
+  }
+  _deleteFriend(Profile user){
+    //remove friend from local storage
+    allFriends.remove(user);
+  }
+
+  _showDeleteFriendAlert(BuildContext context, Profile user){
+    showDialog(
+        barrierDismissible: true,
+        context: context,
+        builder: (context){
+          return AlertDialog(
+            title: Text("Delete Friend"),
+            content: Text("Are you sure you want to delete ${user.userName}?"),
+            actions: [
+              TextButton(
+                  onPressed: (){
+                    setState(() {
+                      _deleteFriend(user);
+                    });
+                    Navigator.of(context).pop();
+                  },
+                  child: Text("Yes")
+              ),
+              TextButton(
+                  onPressed: (){
+                    Navigator.of(context).pop();
+                  },
+                  child: Text("No")
+              )
+            ],
+          );
+        }
     );
   }
 }
