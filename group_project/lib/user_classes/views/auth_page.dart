611ca5c3a05ef539913d542608260e142219c1b2
MatthewSharpOTU <matthew.sharp1@ontariotechu.net>
2022-11-09 00:29:54 -0500

    Page to help redirect Sign-up and Log-in

---
+++ group_project/lib/user_classes/views/auth_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'signup_page.dart';
+import '../../main.dart';
+import '../models/authenticate_user.dart';
+
+class AuthPage extends StatefulWidget {
+  const AuthPage({Key? key, required this.title}) : super(key: key);
+
+  final Widget title;
+
+  @override
+  State<AuthPage> createState() => _AuthPageState();
+}
+
+class _AuthPageState extends State<AuthPage> {
+
+  bool isLogin = true;
+
+  @override
+  Widget build(BuildContext context) => isLogin
+      ? LoginWidget(title: logo, onClickedSignUp: toggle,)
+      : SignUpForm(onClickedSignIn: toggle);
+
+  void toggle() => setState(() => isLogin = !isLogin);
+}
